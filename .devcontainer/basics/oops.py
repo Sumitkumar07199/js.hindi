489567bda97ac87d147a81680dd7e9836d98f2da
Sumit kumar <148640141+Sumitkumar07199@users.noreply.github.com>
2023-10-21 16:39:58 +0000

    OOPS

---
+++ .devcontainer/basics/oops.py
@@ -0,0 +1,35 @@
+#To improve security performance of code, reuseiability development of code.
+#Methodology of code.
+#Oops is amethodology and techniques to devloped real thinking implementation in programming is called oops(object oriented programming system).
+
+#Question: Why we use OOPS:
+#Performance based, Attructured programming object base (reference)programming,dynamic programming.
+# OOPS components:
+# 1-class
+# 2-Methods
+# 3-Attributes(variables)
+# 4-Objects
+
+# 1-Class:
+# Class is a blueprint and prototype we can declare all data member. Function inside in a class.
+    
+# 2-Method:
+# Method is a member of your class. Methods are usefull to perform a logic of in your class.locals
+
+# 3-Attributes:
+# Attributes is a method of class is also called class variable which is decleared inside in class outside of method.
+
+# 4-Object:
+# Object is a method of real world identity/entity of class, object is a current instence(reference) of your class. Object are usefull to allocate dynamic memory of class. 
+
+
+# Question-What is the diffrence between function and method.
+# Function without object wuthout members is called function and that function call in the class is called method.
+# class one():(class)
+#     x=10(variable/attributes)
+#     y=20
+#     def add (self):(method)
+#         z=self.x+self.y
+#         print(z)
+# k=one()(object k is a instence variable)
+# k.add()
